73cd99ee071798e01c6e3073b4acdef6ec4caca3
james <32747767+jamesbond448@users.noreply.github.com>
2024-02-18 01:04:56 -0500

    Update Remove Facebook Comment Annoyance-1.0.user.js

--- Remove Facebook Comment Annoyance-1.0.user.js
+++ Remove Facebook Comment Annoyance-1.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Facebook Comment Annoyance
 // @namespace    http://tampermonkey.net/
-// @version      1.01
+// @version      1.02
 // @description  Remove comment with specific keyword
 // @author       PA
 // @homepage     https://github.com/jamesbond448/facebook_moderation_comment
